2fb55dc89e32768bf44eb5e08fd1ef776bfdf5a8
cirnicirno <cirnicirno@users.noreply.github.com>
2018-05-29 17:37:43 +0300

    Add class "Multitabs".

---
+++ src/Lab1.java
@@ -0,0 +1,7 @@
+public class Lab1 {
+    public static void main(String[] args)
+    {
+        Multitabs multitabs = new Multitabs();
+        multitabs.printconsole();
+    }
+}
--- src/Multitabs.java
+++ src/Multitabs.java
@@ -1,8 +1,8 @@
-public class lab1 {
-    public static void main(String[] args)
-    {
-        for (int x = 1; x<=10; x++){
-            for (int y = 1; y<=10; y++){
+public class Multitabs
+{
+    public void printconsole(){
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 10; y++) {
                 int result = x * y;
                 System.out.printf("%d * %d = %d\n", x, y, result);
             }
