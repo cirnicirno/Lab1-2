a9e2ab4483160050a409941ba96a3028a481d31a
cirnicirno <cirnicirno@users.noreply.github.com>
2018-05-29 18:10:40 +0300

    Add constructor

--- src/MultiTabs.java
+++ src/MultiTabs.java
@@ -1,6 +1,11 @@
 public class MultiTabs
 {
+
     private int result;
+
+    public MultiTabs() {
+    }
+
     public void PrintConsole(){
         for (int x = 1; x <= 10; x++) {
             for (int y = 1; y <= 10; y++) {
